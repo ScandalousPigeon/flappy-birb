8de08f5bf8ee41f2d8f3157196b90416e4254bcc
ScandalousPigeon <951413eelnoel@gmail.com>
2026-03-16 20:20:21 +1000

    changed windows size, title

--- src/main/java/com/example/flappybird/FlappyApp.java
+++ src/main/java/com/example/flappybird/FlappyApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class FlappyApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(FlappyApp.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Flappy Birb");
         stage.setScene(scene);
         stage.show();
     }
